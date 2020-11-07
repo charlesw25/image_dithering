1fa40f988ae1cd5535d3094d006b60a8efa6ca31
charlesw25 <woodcharles544@gmail.com>
2020-11-06 21:21:44 -0500

    Implemented basic algorithm for Ordered Dithering

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
 import numpy as np
 import cv2
+import time
 
-img = cv2.imread('test.jpg', 0)
-# cv2.imshow('image',img)
-# k = cv2.waitKey(0)
-# if k == 27:         # wait for ESC key to exit
-#     cv2.destroyAllWindows()
-# elif k == ord('s'): # wait for 's' key to save and exit
-#     cv2.imwrite('testgray.png',img)
-#     cv2.destroyAllWindows()
-#print(np.shape(img))
-print(img[0][0])
-x = len(img[0])
-y = len(img)
-for i in range(y):
-     for j in range(x):
-         oldPixel = img[i][j]
-         newPixel = int(oldPixel / 256)
-         img[i][j] = oldPixel
-         quantError = oldPixel - newPixel
-         print(quantError)
-         if j + 1 < x:
-             img[i][j+1] = img[i][j+1] + quantError * (7/16)
-         if j - 1 >= 0 and i + 1 < y:
-             img[i+1][j-1] = img[i+1][j-1] + quantError * (3/16)
-         if i + 1 < y:
-             img[i+1][y] = img[i+1][y] + quantError * (5/16)
-         if j + 1 < x and i + 1 < y:
-             img[i+1][y+1] = img[i+1][y+1] + quantError * (1/16)
+# https://www.visgraf.impa.br/Courses/ip00/proj/Dithering1/ordered_dithering.html
+def orderedDitheringBW(name, n = 2):
+    img = cv2.imread(name, 0)
+    height, width = img.shape
+
+    thresh = np.array([[.25, .75], [1.0, .05]])
+
+    for i in range(height): # y
+        for j in range(width): # x
+            if img[i][j]/255 < thresh[i%n][j%n]:
+                img[i][j] = 0
+            else:
+                img[i][j] = 255
 
 
+    cv2.imshow('a', img)
+    cv2.waitKey()
+    return img
 
-cv2.imwrite('test.png',img)
+# Based off: https://en.wikipedia.org/wiki/Ordered_dithering
+def orderedDithering(name, n = 2):
+    img = cv2.imread(name)
+    height, width, channels = img.shape
+
+    thresh = np.array([[.25, .75], [1.0, .05]])
+
+    r = 255/channels
+    offset = 0.5
+
+    for i in range(height): # y
+        for j in range(width): # x
+            blu, grn, red = img[i][j]
+
+            newB = findClosestColor(blu + r * (thresh[i%n][j%n] - offset))
+            newG = findClosestColor(grn + r * (thresh[i%n][j%n] - offset))
+            newR = findClosestColor(red + r * (thresh[i%n][j%n] - offset))
+
+            img[i][j] = (newB, newG, newR)
+
+    return img
+
+def findClosestColor(px, values = [0, 128, 255]):
+    dist = list(map(lambda x: abs(x-px), values))
+    idx = dist.index(min(dist))
+    return values[idx]
+
+if __name__ == '__main__':
+    start_time = time.time()
+    # orderedDitheringBW('chonk.png')
+    res = orderedDithering('chonk.png')
+    print(f'--- Time: {time.time()-start_time}sec ---')
+    cv2.imwrite('dither.png', res)
+    # cv2.imshow('ditha', res)
+    # cv2.waitKey()
